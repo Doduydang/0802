cd63652765652d68124eb9c9b5387e9fd9154d2a
Doduydang <doduydang@gmail.com>
2020-08-02 23:32:42 +0700

    First Commit
    This is first commit to test GitHub

---
+++ mytrial/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import RoutesComponent from "./view/shared/routes/RoutesComponent";
+
+function App() {
+  return (
+    <div>
+      <h1>This is my 2nd</h1>
+      <RoutesComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ mytrial/src/view/shared/routes/RoutesComponent.js
@@ -0,0 +1,10 @@
+//import library
+import React, { Component } from "react";
+//import Component
+
+class RoutesComponent extends Component {
+  render() {
+    return <h1>Hello Everyone</h1>;
+  }
+}
+export default RoutesComponent;
